511d5502567611ebfde1048db37c334b2a65df4d
“TechnourceDeveloper” <krishita@technource.com>
2023-07-03 18:06:25 +0530

    Add Welcome screen functionality

---
+++ app/src/main/java/com/technource/android/ui/loginModule/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.technource.android.ui.loginModule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_kotlin_boilerplate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/technource/android/ui/registrationModule/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.technource.android.ui.registrationModule
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.android_kotlin_boilerplate.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
