0ba1c3eac1dd63b9f7e8592d45dcbe942c419413
“TechnourceDeveloper” <krishita@technource.com>
2023-07-04 18:37:40 +0530

    More screen integration

--- app/src/main/java/com/technource/android/utils/Constant.kt
+++ app/src/main/java/com/technource/android/utils/Constant.kt
@@ -5,5 +5,8 @@ class Constants {
     companion object {
         const val APP_SHARED_PREFERENCE_NAME = "android_boilerplate_preference"
         const val gitRepo = "https://github.com/TechnourceDeveloper/Android-Kotlin-Boilarplate"
+        const val aboutUs = "https://www.technource.com/services/"
+        const val termsNCondition = "https://www.technource.com/terms-conditions/"
+        const val privacyPolicy = "https://technource.com/privacy-policy/"
     }
 }
