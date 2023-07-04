84bf10c0d29a868afb38a4fb392f78606d44e2dc
“TechnourceDeveloper” <krishita@technource.com>
2023-07-04 18:21:29 +0530

    More screen integration

---
+++ app/src/main/java/com/technource/android/ui/moreModule/MoreActivity.kt
@@ -0,0 +1,55 @@
+package com.technource.android.ui.moreModule
+
+import android.content.Intent
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.android_kotlin_boilerplate.R
+import com.example.android_kotlin_boilerplate.databinding.ActivityMoreBinding
+import com.technource.android.base.BaseActivity
+import com.technource.android.commonInterface.RecyclerviewInterface
+import com.technource.android.ui.viewTermsModule.TermsViewActivity
+
+class MoreActivity : BaseActivity<ActivityMoreBinding>() {
+    override fun getViewBinding() = ActivityMoreBinding.inflate(layoutInflater)
+    private lateinit var moreListAdapter: MoreListAdapter
+    val moreModel = ArrayList<MoreModel>()
+    private lateinit var moreViewModel: MoreViewModel
+    override fun initObj() {
+        // Initialize MoreViewModel instance
+        moreViewModel = ViewModelProvider(this)[MoreViewModel::class.java]
+
+        // Set viewModel and lifecycle owner for data binding
+        binding.viewModel = moreViewModel
+        binding.lifecycleOwner = this
+
+        // Set the app bar text to "More"
+        binding.appbar.appBarText.text = resources.getString(R.string.more)
+
+        // Create and set up the MoreListAdapter
+        moreListAdapter = MoreListAdapter()
+        binding.rvMoreList.layoutManager = LinearLayoutManager(this)
+        binding.rvMoreList.adapter = moreListAdapter
+
+        // Observe the list of items from the MoreViewModel
+        moreViewModel.getList().observe(this) { moreList ->
+            moreListAdapter.setData(moreList)
+            moreModel.addAll(moreList)
+        }
+
+        // Set item click listener for the MoreListAdapter
+        moreListAdapter.setOnItemClick(object : RecyclerviewInterface {
+            override fun onItemClick(position: Int) {
+                val intent = Intent(this@MoreActivity, TermsViewActivity::class.java)
+                intent.putExtra("url", moreModel[position].url)
+                startActivity(intent)
+                overridePendingTransition(R.anim.slide_in_up, R.anim.nothing_ani)
+            }
+        })
+    }
+
+    override fun click() {
+        binding.appbar.back.setOnClickListener {
+            finish()
+        }
+    }
+}
