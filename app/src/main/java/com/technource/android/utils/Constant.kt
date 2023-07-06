43bd978c359712b1a762a1b386642448fd7529a3
“TechnourceDeveloper” <krishita@technource.com>
2023-07-06 10:02:02 +0530

    Add comments in functions and change more screens variable name

--- app/src/main/java/com/technource/android/utils/Constant.kt
+++ app/src/main/java/com/technource/android/utils/Constant.kt
@@ -9,8 +9,9 @@ class Constants {
         const val IS_INTRO_SCREEN_DONE = "is_intro_screen_done"
         const val IS_LOGGED_IN = "is_logged_in"
         const val GIT_REPO_LINK = "https://github.com/TechnourceDeveloper/Android-Kotlin-Boilarplate"
-        const val aboutUs = "https://www.technource.com/services/"
-        const val termsNCondition = "https://www.technource.com/terms-conditions/"
-        const val privacyPolicy = "https://technource.com/privacy-policy/"
+        const val ABOUT_US_LINK = "https://www.technource.com/services/"
+        const val TERMS_CONDITION_LINK = "https://www.technource.com/terms-conditions/"
+        const val PRIVACY_POLICY_LINK = "https://technource.com/privacy-policy/"
+        const val INTENT_KEY_URL = "url"
     }
 }
