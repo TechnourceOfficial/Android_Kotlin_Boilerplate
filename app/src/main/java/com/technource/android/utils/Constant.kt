95f95a40bd4ed561e7a8895d526e8af19050ba0d
“TechnourceDeveloper” <krishita@technource.com>
2023-07-06 17:38:34 +0530

    Add application video and change github link

--- app/src/main/java/com/technource/android/utils/Constant.kt
+++ app/src/main/java/com/technource/android/utils/Constant.kt
@@ -8,7 +8,7 @@ class Constants {
         const val LANGUAGE_CODE = "language_code"
         const val IS_INTRO_SCREEN_DONE = "is_intro_screen_done"
         const val IS_LOGGED_IN = "is_logged_in"
-        const val GIT_REPO_LINK = "https://github.com/TechnourceDeveloper/Android-Kotlin-Boilarplate"
+        const val GIT_REPO_LINK = "https://github.com/TechnourceOfficial/Android-Kotlin-Boilarplate"
         const val ABOUT_US_LINK = "https://www.technource.com/services/"
         const val TERMS_CONDITION_LINK = "https://www.technource.com/terms-conditions/"
         const val PRIVACY_POLICY_LINK = "https://technource.com/privacy-policy/"
